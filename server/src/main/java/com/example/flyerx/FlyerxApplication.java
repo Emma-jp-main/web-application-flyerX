e770450df2e7cc2a54e91540777d3c3223270925
Emma-jp-main <mathlogue3431@gmail.com>
2026-06-27 21:33:08 +0900

    feat: 環境構築をした

---
+++ server/src/main/java/com/example/flyerx/FlyerxApplication.java
@@ -0,0 +1,13 @@
+package com.example.flyerx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlyerxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlyerxApplication.class, args);
+	}
+
+}
